7babe1b011274cc996205515e3d578ffefbced8e
Abraham <aromeroa1500@alumno.ipn.mx>
2022-07-30 00:31:30 -0500

    Clase 14. Peticiones Asíncronas (AJAX y API's)

--- src/components/CicloVida.js
+++ src/components/CicloVida.js
@@ -6,7 +6,7 @@ import React, {Component} from "react";
     }
 
     componentWillUnmount(){//Este metodo se ejecuta cuando el componente ya no exista
-        console.log(3, "El componente ha sido eliminado del DOM");
+        //console.log(3, "El componente ha sido eliminado del DOM");
     };
 
     render(){
@@ -18,7 +18,7 @@ import React, {Component} from "react";
     constructor(props){
         super(props);
 
-        console.log(0, "El componente se inicializa, AUN NO ESTA EN EL DOM");
+        //console.log(0, "El componente se inicializa, AUN NO ESTA EN EL DOM");
     
         this.state = {
             hora : new Date().toLocaleTimeString(),
@@ -29,13 +29,13 @@ import React, {Component} from "react";
     };
 
     componentDidMount(){
-        console.log(1, "El componente ya se encuentra en el DOM");
+        //console.log(1, "El componente ya se encuentra en el DOM");
     };
 
     componentDidUpdate(prevProps, prevState){
-        console.log(2, "El estado o las props del componente han cambiado");
-        console.log(prevProps);
-        console.log(prevState);
+        //console.log(2, "El estado o las props del componente han cambiado");
+        //console.log(prevProps);
+        //console.log(prevState);
     };
 
 
@@ -63,7 +63,7 @@ import React, {Component} from "react";
 
     
     render(){
-        console.log(4, "El componente se dibuja o redibuja por algun cambio en el DOM");
+        //console.log(4, "El componente se dibuja o redibuja por algun cambio en el DOM");
         return (
             <>
                 <h2> Ciclo de vida de los Componentes</h2>
