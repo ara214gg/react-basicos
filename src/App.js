6fc016da841f11586f62a951ad798a0af8526e9a
Abraham <aromeroa1500@alumno.ipn.mx>
2022-08-04 18:21:17 -0500

    Clase 21. Referencias

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ScrollHooks from './components/ScrollHooks';
 import RelojHooks from './components/RelojHooks';
 import AjaxHooks from './components/AjaxHooks';
 import HooksPersonalizados from './components/HooksPersonalizados';
+import Referencias from './components/Referencias';
 
 function App() {
   return (
@@ -72,6 +73,8 @@ function App() {
         <AjaxHooks/>
         <hr/>
         <HooksPersonalizados/>
+        <hr/>
+        <Referencias/>
         <hr/>
       </section>
       </header>
