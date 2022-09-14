0ee3a00dd0bd313725b6f784f89dd8ec249a3a9c
Abraham <aromeroa1500@alumno.ipn.mx>
2022-09-14 18:02:05 -0500

    Clase 23 styles

--- src/components/Estilos.js
+++ src/components/Estilos.js
@@ -3,17 +3,17 @@ import "./Estilos.css"
 
 export default function Estilos(){
     let myStyles = {
-        borderRadius : ".5rem",
-        margin : "2rem auto",
-        maxWidth : "50%"
+        borderRadius:".5rem",
+        margin:"2rem auto",
+        maxWidth:"50%"
     };
 
     return(
         <section className='estilos'>
             <h2> Estilos en React </h2>    
             <h3 className='bg-react'> Estilos CSS en hoja externa </h3>     
-            <h3 style={ {borderRadius: ".25rem", margin: "1rem"} }> Estilos en línea (atributo style) </h3>     
-            <h3 style = {{myStyles}}> Estilos en línea </h3>     
+            <h3 className='bg-react' style = { {borderRadius : "0.25rem", margin : "1rem"} }> Estilos en linea </h3>     
+            <h3 className='bg-react' style = {myStyles}> Estilos en línea </h3>     
         </section>
     );
 }
