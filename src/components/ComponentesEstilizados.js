1cb582a679d72acbfff9b6b87a1900ad5e9dd3f8
Abraham <aromeroa1500@alumno.ipn.mx>
2022-09-14 18:19:28 -0500

    styled-components

---
+++ src/components/ComponentesEstilizados.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function ComponentesEstilizados(){
+    const MyH3 = styled.h3`
+        
+    `;
+
+    return(
+        <>
+            <h2> Styled Components</h2>
+            <h3> Hola sou un h3 estilizado con Styled-components</h3>
+        </>
+    );
+}
